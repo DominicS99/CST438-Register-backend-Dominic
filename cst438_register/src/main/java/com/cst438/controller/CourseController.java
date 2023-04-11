36b35726ab0412874a975a54799097acd6e4e89d
Dominic <dsagen@csumb.edu>
2023-04-11 06:43:57 -0700

    Update grades for course

--- cst438_register/src/main/java/com/cst438/controller/CourseController.java
+++ cst438_register/src/main/java/com/cst438/controller/CourseController.java
@@ -25,6 +25,11 @@ public class CourseController {
 	public void updateCourseGrades( @RequestBody CourseDTOG courseDTO, @PathVariable("course_id") int course_id) {
 		
 		//TODO  complete this method in homework 4
+		for (CourseDTOG.GradeDTO grade : courseDTO.grades) {
+			Enrollment currEnroll = enrollmentRepository.findByEmailAndCourseId(grade.student_email, course_id);
+			currEnroll.setCourseGrade(grade.grade);
+			enrollmentRepository.save(currEnroll);
+		}
 		
 	}
 
